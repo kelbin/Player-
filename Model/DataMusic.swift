61e4e75ab005ec9c7e1580064d19c9dee17cb6c5
kelbin <kaeltero@gmail.com>
2017-07-12 20:18:43 +0300

    Player

---
+++ Model/DataMusic.swift
@@ -0,0 +1,17 @@
+//
+//  DataMusic.swift
+//  Player
+//
+//  Created by Келбин on 12.07.17.
+//  Copyright © 2017 Келбин. All rights reserved.
+//
+
+import Foundation
+
+struct DataMusic {
+    var name:String
+    var music:String
+    var genre:String
+    var album:String?
+    var author:String?
+}
---
+++ Model/Service.swift
@@ -0,0 +1,12 @@
+//
+//  Service.swift
+//  Player
+//
+//  Created by Келбин on 12.07.17.
+//  Copyright © 2017 Келбин. All rights reserved.
+//
+
+import Foundation
+
+
+
