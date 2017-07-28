3121e105e5ef84b3c1cd4df5a732dac458080c8d
kelbin <kaeltero@gmail.com>
2017-07-28 17:40:59 +0300

    Player

--- Player/AppDelegate.swift
+++ Player/AppDelegate.swift
@@ -13,19 +13,18 @@ import AVFoundation
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        audioInit()
         return true
     }
     
-    func audioInit() -> Bool {
+    func audioInit() {
         let audioSession = AVAudioSession.sharedInstance()
         
         do {
             try audioSession.setCategory(AVAudioSessionCategoryPlayback)
-        }
-        catch {
+        } catch {
             print("Setting category to AVAudioSessionCategoryPlayback failed.")
         }
-        return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
